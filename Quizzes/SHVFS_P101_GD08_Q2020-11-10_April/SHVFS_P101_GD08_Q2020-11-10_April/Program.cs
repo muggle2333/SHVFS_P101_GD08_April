ed6e691d4680b9a31e4116d3c7f73b5288f82912
muggle2333 <114428669+muggle2333@users.noreply.github.com>
2022-11-11 18:15:20 +0800

    Add files via upload

---
+++ Quizzes/SHVFS_P101_GD08_Q2020-11-10_April/SHVFS_P101_GD08_Q2020-11-10_April/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace SHVFS_P101_GD08_Q2_April
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string guess = " ";
+            string[] array = new string[]{"hello","world","Chris"};
+            Random random = new Random();
+            int i = random.Next(array.Length);
+            int time = 0;
+            Console.WriteLine("Hello,welcome!What's your name?");
+            string name = Console.ReadLine();
+            Console.Clear();
+            while (guess != array[i])
+            {
+                Console.WriteLine($"{name}, Guess a word !");
+                guess = Console.ReadLine();
+                //if (guess == array[i])
+                //{
+                //    Console.WriteLine("Right");
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Wrong!Again");
+                //}
+                Console.Clear();
+                time++;
+            }
+            Console.Clear();
+            Console.WriteLine($"Right,you have guessed {time} times!");
+
+        }
+    }
+}
