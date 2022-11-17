9b3afa961779bbe2d126dd56d9ea95c056d2817b
muggle2333 <114428669+muggle2333@users.noreply.github.com>
2022-11-17 19:28:52 +0800

    Add files via upload

---
+++ Classes/SHVFS_P101_GD08_C7/SHVFS_P101_GD08_C2_Apri_Snakel/SHVFS_P101_GD08_C2_Apri_Snakel/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHVFS_P101_GD08_C2_Apri_Snakel
+{
+    public class Player
+    {
+        public Vector2 Position;
+        public Vector2 Direction;
+    }
+}
---
+++ Classes/SHVFS_P101_GD08_C7/SHVFS_P101_GD08_C2_Apri_Snakel/SHVFS_P101_GD08_C2_Apri_Snakel/Vector2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHVFS_P101_GD08_C2_Apri_Snakel
+{
+    public struct Vector2
+    {
+        //this is "small" inconsistency/code style
+        //Mathematics libraries usually use camel instead of UpperCamer
+        public int x;
+        public int y;
+
+        public Vector2(int x, int y)
+        {
+            //the keyword "this" means this object when an instance is instantiated using the keyword "new"
+            this.x = x; //can't be the same so use "this"
+            this.y = y;
+        }
+    }
+}
